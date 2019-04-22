389feb33a8b4985753244a0fc65115a8972072cb
Yan Zhao <dongnanzhy@gmail.com>
2019-04-21 21:42:44 -0700

    add express, change web content

---
+++ server.js
@@ -0,0 +1,53 @@
+// Get the dependencies
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Point static path to dist -- For building -- REMOVE
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'dist/hello-world')));
+
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+const port = process.env.PORT || '3200';
+app.set('port', port);
+
+
+// Create HTTP server
+const server = http.createServer(app);
+server.listen( port , () => console.log('Running on port 3200'));
+
+/*============
+var connectionString = 'mongodb://127.0.0.1:27017/webdev';
+//var connectionString = 'mongodb://the string provided by mlab';
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const client = mongoose.connect( connectionString, { useNewUrlParser: true });
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+require('./assignment/app')(app);
+===============*/
+
+//demo refactor
+//require('./helloworld')(app);
+//require('./assignment/app-demo')(app);
+//require('./assignment/app-demo-two')(app);
+//require('./demo/app')(app);
